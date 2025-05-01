ea82bf27fecfc132e5ba1bf36ffa0e092bf8a370
DXgriz <vuyaningcobos@gmail.com>
2025-05-01 07:19:24 +0200

    updating Controllers /s

--- SmartCampus.API/obj/Debug/net9.0/SmartCampus.API.AssemblyInfo.cs
+++ SmartCampus.API/obj/Debug/net9.0/SmartCampus.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartCampus.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e79339d312d6d7d278b54bd3ef38fda1db4f405")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1e1d95fe4404db1b630404d5116751b08453c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartCampus.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartCampus.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
